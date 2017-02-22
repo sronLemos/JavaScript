cec006acd48a7e3f38893b4b3de018422a010129
sronLemos <sron.lemos@gmail.com>
2017-02-21 22:59:09 -0300

    js

---
+++ teste.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 1;
+
+var resultado = a + b;
+
+console.log('Resultado: ', + resultado);
---
+++ variaveis_e_hoisting/variaveis_e_hoisting.js
@@ -0,0 +1,16 @@
+var percentualImposto = 0.15;
+valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar ', totalImposto);
+
+var valor;
+// HOISTING - todas as declarações de variáveis ficam em cima.
+/*
+var percentualImposto, valor, totalImposto;
+percentualImposto = 0.15;
+valor = 100;
+totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+*/
